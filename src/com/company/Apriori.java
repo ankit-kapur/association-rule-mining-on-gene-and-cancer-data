5e985b01bdedc28ff05ba5a6d2f77e90acf1febd
ankit-kapur <ankitkap@buffalo.edu>
2015-09-30 06:58:45 -0400

    Association ruling mining complete.

--- src/com/company/Apriori.java
+++ src/com/company/Apriori.java
@@ -7,20 +7,58 @@ import java.util.*;
 
 public class Apriori {
 
+    static final int SUPPORT = 40;
+    static final int CONFIDENCE = 70;
+
     public static void main(String[] args) {
 
         String filePath = "/home/castamere/code/datamining/hw2/association-rule-test-data.txt";
         List<Map<String, Boolean>> data = readData(filePath);
 
-        Map<Set<String>, Integer> frequentItemset = runApriori(data, 50);
+        Map<Set<String>, Integer> frequentItemset = runApriori(data, SUPPORT);
+        System.out.println("----------------------------------------");
         System.out.println("Frequent itemset ===> Size = " + frequentItemset.size() + " ==> " + frequentItemset);
 
-        List<Map<String, String>> associationRules = generateAssociationRules(frequentItemset);
-        System.out.println("\nAssociationRules ===> Size = " + associationRules.size() + " ==> " + associationRules);
+        List<String> associationRules = generateAssociationRules(frequentItemset, CONFIDENCE);
+        System.out.println("Association rules ===> Size = " + associationRules.size() + " ==> " + associationRules);
+
+        
     }
 
-    private static List<Map<String, String>> generateAssociationRules(Map<Set<String>, Integer> frequentItemset) {
-        return null;
+    private static List<String> generateAssociationRules(Map<Set<String>, Integer> frequentItemset, int minConfidence) {
+        List<String> associationRules = new ArrayList<>();
+
+        for (Set<String> L : frequentItemset.keySet()) {
+            for (Set<String> S : generatePowerSet(L)) {
+                if (S.size() > 0 && S.size() < L.size() && frequentItemset.containsKey(S) && frequentItemset.get(S) > 0) {
+                    double supportCountL = frequentItemset.get(L);
+                    double supportCountS = frequentItemset.get(S);
+                    double confidence = 100.0 * (supportCountL / supportCountS);
+                    if (confidence >= minConfidence) {
+                        Set<String> LminusS = new HashSet<>(L);
+                        LminusS.removeAll(S);
+                        associationRules.add(S + " ==> " + LminusS);
+                    }
+                }
+            }
+        }
+
+        return associationRules;
+    }
+
+    private static <String> Set<Set<String>> generatePowerSet(Set<String> theGivenSet) {
+        Set<Set<String>> thePowerset = new HashSet<>();
+        List<String> list = new ArrayList<>(theGivenSet);
+        String head = list.get(0);
+        Set<String> rest = new HashSet<>(list.subList(1, list.size()));
+        for (Set<String> thisSet : generatePowerSet(rest)) {
+            Set<String> newSet = new HashSet<>();
+            newSet.add(head);
+            newSet.addAll(thisSet);
+            thePowerset.add(newSet);
+            thePowerset.add(thisSet);
+        }
+        return thePowerset;
     }
 
     private static Map<Set<String>, Integer> runApriori(List<Map<String, Boolean>> data, int minSupport) {
@@ -66,7 +104,7 @@ public class Apriori {
         }
 
         long end = System.currentTimeMillis();
-        System.out.println("\n\nTotal execution time is: " + ((double) (end - start) / 1000) + " seconds.");
+        System.out.println("\nTotal execution time is: " + ((double) (end - start) / 1000) + " seconds.");
         return resultSet;
     }
 
